4ad0d2933f42d09bbf0d048b15fd3000ec8a0352
ajaysharmaaaaa <ajaysharma992805@gmail.com>
2025-09-26 09:28:49 +0530

    Fix process is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window',
+    process: {
+      env: {},
+      stdout: { isTTY: false },
+      stderr: { isTTY: false },
+      platform: 'browser',
+      version: '',
+      versions: { node: '' },
+      cwd: () => '/',
+      nextTick: (cb: Function) => setTimeout(cb, 0)
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
